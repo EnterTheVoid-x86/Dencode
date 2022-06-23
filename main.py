6189a76bf03a75229d215613c025431949b95e76
etvx86 <64978924+EnterTheVoid-x86@users.noreply.github.com>
2022-06-23 13:39:10 -0400

    Dencode v1.1

--- main.py
+++ main.py
@@ -9,9 +9,7 @@ token = "your token here!"
 
 status = "bytes of ascii into base64, binary, and more!"
 
-bot = commands.Bot(command_prefix='=')
-
-
+bot = commands.Bot(command_prefix='=', help_command=None)
 
 @bot.event
 async def on_ready():
@@ -19,7 +17,13 @@ async def on_ready():
     print(bot.user.name)
     print(bot.user.id)
     print('------')
-    await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.streaming, name = status))
+    await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.playing, name = status))
+@bot.event
+async def on_command_error(ctx, error):
+    if isinstance(error, commands.CommandNotFound):
+        await ctx.send("```Command not found.```")
+    if isinstance(error, commands.MissingRequiredArgument):
+        await ctx.send("```Missing required argument.```")
 @bot.command()
 async def encode64(ctx, *, message):
     """Encode a message to base64"""
@@ -53,13 +57,27 @@ async def decodehex(ctx, *, message):
     res = ''.join(chr(int(message[i:i+2], 16)) for i in range(0, len(message), 2))
     await ctx.send("```Decoded output: " + res + "```")
 @bot.command()
+async def experimentalencodeoct(ctx, *, message):
+    """Encode a message to octal"""
+    message = message.encode('utf-8').decode('utf-8')
+    res = ''.join(format(ord(i), '02o') for i in message)
+    await ctx.send("```Encoded output: " + res + "```")
+@bot.command()
+async def experimentaldecodeoct(ctx, *, message):
+    """Decode a message from octal"""
+    message = message.encode('utf-8').decode('utf-8')
+    res = ''.join(chr(int(message[i:i+3], 8)) for i in range(0, len(message), 3))
+    await ctx.send("```Decoded output: " + res + "```")
+@bot.command()
 async def info(ctx):
     """Info about the bot"""
-    await ctx.send("```This bot was made by @Nucleus#1922.\nThe source code can be found here: https://github.com/EnterTheVoidx86/dencode```")
-@bot.event
-async def on_command_error(ctx, error):
-    if isinstance(error, commands.CommandNotFound):
-        await ctx.send("```Command not found.```")
-    if isinstance(error, commands.MissingRequiredArgument):
-        await ctx.send("```Missing required argument.```")
+    await ctx.send("```This bot was made by @Nucleus#1922.\nThe source code can be found here: https://github.com/EnterTheVoid-x86/Dencode```")
+@bot.command()
+async def ping(ctx):
+    """Pong!"""
+    await ctx.send("```Pong!```")
+@bot.command()
+async def help(ctx):
+    """Help command"""
+    await ctx.send("```Available commands:\n\n=encode64 <message>\n=decode64 <message>\n=encodebin <message>\n=decodebin <message>\n=encodehex <message>\n=decodehex <message>\n=experimentalencodeoct <message>\n=experimentaldecodeoct <message>\n=info\n=ping\n=help```")
 bot.run(token)
