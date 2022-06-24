4b4a1f7322a20c1ada9dff823908a661a2175ee3
etvx86 <64978924+EnterTheVoid-x86@users.noreply.github.com>
2022-06-24 10:12:36 -0400

    Dencode v1.4

--- main.py
+++ main.py
@@ -6,12 +6,17 @@ from discord.ext import commands
 import base64
 import datetime
 import os
+import string
 
 token = "your token here!"
 
 status = "bytes of ascii into base64, binary, and more! Prefix is =."
 
+botver = "1.4"
+
 bot = commands.Bot(command_prefix='=', help_command=None)
+
+
 
 @bot.event
 async def on_ready():
@@ -109,6 +114,15 @@ async def decode85(ctx, *, message):
     print("[DECODE85] " + str(datetime.datetime.now()))
     await ctx.send("```Decoded output: " + base64.b85decode(message.encode('utf-8')).decode('utf-8') + "```")
 @bot.command()
+async def encodenum(ctx, *, message):
+    num_list = [ord(x) - 96 for x in message]
+    print("[ENCODENUM] " + str(datetime.datetime.now()))
+    await ctx.send("```Encoded output: " + str(num_list) + "```")
+@bot.command()
+async def decodenum(ctx):
+    print("[DECODENUM] " + str(datetime.datetime.now()))
+    await ctx.send("```Not yet supported!```")
+@bot.command()
 async def info(ctx):
     print("[INFO] " + str(datetime.datetime.now()))
     await ctx.send("```This bot was made by @Nucleus#1922.\nThe source code can be found here: https://github.com/EnterTheVoid-x86/Dencode```")
@@ -117,7 +131,11 @@ async def ping(ctx):
     print("[PING] " + str(datetime.datetime.now()))
     await ctx.send("```Pong!```")
 @bot.command()
+async def version(ctx):
+    print("[VERSION] " + str(datetime.datetime.now()))
+    await ctx.send("```Version: " + botver + "```")
+@bot.command()
 async def help(ctx):
     print("[HELP] " + str(datetime.datetime.now()))
-    await ctx.send("```Available commands:\n\n=encode64 <message>\n=decode64 <message>\n=encodebin <message>\n=decodebin <message>\n=encodehex <message>\n=decodehex <message>\n=experimentalencodeoct <message>\n=experimentaldecodeoct <message>\n=encode32 <message>\n=decode32 <message>\n=encode16 <message>\n=decode16 <message>\n=encodea85 <message>\n=decodea85 <message>\n=encode85 <message>\n=decode85 <message>\n=info\n=ping\n=help\n\nOctal encoding/decoding is experimental, and still has many bugs. For example,\nmessages encoded or decoded with spaces won't encode or decode properly.```")
+    await ctx.send("```Available commands:\n\n=encode64 <message>\n=decode64 <message>\n=encodebin <message>\n=decodebin <message>\n=encodehex <message>\n=decodehex <message>\n=experimentalencodeoct <message>\n=experimentaldecodeoct <message>\n=encode32 <message>\n=decode32 <message>\n=encode16 <message>\n=decode16 <message>\n=encodea85 <message>\n=decodea85 <message>\n=encode85 <message>\n=decode85\n=encodenum <message>\n=decodenum <message>\n=info\n=ping\n=help\n\nOctal encoding/decoding is experimental, and still has many bugs. For example,\nmessages encoded or decoded with spaces won't encode or decode properly.```")
 bot.run(token)
