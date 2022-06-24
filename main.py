b63be925e196e956d76bf1c066bb91a101ba1aac
etvx86 <64978924+EnterTheVoid-x86@users.noreply.github.com>
2022-06-23 21:54:48 -0400

    Dencode v1.3

--- main.py
+++ main.py
@@ -4,6 +4,8 @@
 import discord
 from discord.ext import commands
 import base64
+import datetime
+import os
 
 token = "your token here!"
 
@@ -24,67 +26,97 @@ async def on_command_error(ctx, error):
         await ctx.send("```Command not found.```")
     if isinstance(error, commands.MissingRequiredArgument):
         await ctx.send("```Missing required argument.```")
+    if isinstance(error, commands.TooManyArguments):
+        await ctx.send("```Too many arguments.```")
+@bot.command()
+async def debugrebootdontuse(ctx):
+    await ctx.send("```Rebooting...```")
+    os.system("taskkill /f /im py.exe && py main.py")
 @bot.command()
 async def encode64(ctx, *, message):
+    print("[ENCODE64] " + str(datetime.datetime.now()))
     await ctx.send("```Encoded output: " + base64.b64encode(message.encode('utf-8')).decode('utf-8') + "```")
 @bot.command()
 async def decode64(ctx, *, message):
+    print("[DECODE64] " + str(datetime.datetime.now()))
     await ctx.send("```Decoded output: " + base64.b64decode(message.encode('utf-8')).decode('utf-8') + "```")
 @bot.command()
 async def encodebin(ctx, *, message):
+    print("[ENCODEBIN] " + str(datetime.datetime.now()))
     message = message.encode('utf-8').decode('utf-8')
     res = ''.join(format(ord(i), '08b') for i in message)
     await ctx.send("```Encoded output: " + res + "```")
 @bot.command()
 async def decodebin(ctx, *, message):
+    print("[DECODEBIN] " + str(datetime.datetime.now()))
     message = message.encode('utf-8').decode('utf-8')
     res = ''.join(chr(int(message[i:i+8], 2)) for i in range(0, len(message), 8))
     await ctx.send("```Decoded output: " + res + "```")
 @bot.command()
 async def encodehex(ctx, *, message):
+    print("[ENCODEHEX] " + str(datetime.datetime.now()))
     message = message.encode('utf-8').decode('utf-8')
     res = ''.join(format(ord(i), '02x') for i in message)
     await ctx.send("```Encoded output: " + res + "```")
 @bot.command()
 async def decodehex(ctx, *, message):
+    print("[DECODEHEX] " + str(datetime.datetime.now()))
     message = message.encode('utf-8').decode('utf-8')
     res = ''.join(chr(int(message[i:i+2], 16)) for i in range(0, len(message), 2))
     await ctx.send("```Decoded output: " + res + "```")
 @bot.command()
 async def experimentalencodeoct(ctx, *, message):
+    print("[ENCODEOCT] " + str(datetime.datetime.now()))
     message = message.encode('utf-8').decode('utf-8')
     res = ''.join(format(ord(i), '02o') for i in message)
     await ctx.send("```Encoded output: " + res + "```")
 @bot.command()
 async def experimentaldecodeoct(ctx, *, message):
+    print("[DECODEOCT] " + str(datetime.datetime.now()))
     message = message.encode('utf-8').decode('utf-8')
     res = ''.join(chr(int(message[i:i+3], 8)) for i in range(0, len(message), 3))
     await ctx.send("```Decoded output: " + res + "```")
 @bot.command()
 async def encode32(ctx, *, message):
+    print("[ENCODE32] " + str(datetime.datetime.now()))
     await ctx.send("```Encoded output: " + base64.b32encode(message.encode('utf-8')).decode('utf-8') + "```")
 @bot.command()
 async def decode32(ctx, *, message):
+    print("[DECODE32] " + str(datetime.datetime.now()))
     await ctx.send("```Decoded output: " + base64.b32decode(message.encode('utf-8')).decode('utf-8') + "```")
 @bot.command()
 async def encode16(ctx, *, message):
+    print("[ENCODE16] " + str(datetime.datetime.now()))
     await ctx.send("```Encoded output: " + base64.b16encode(message.encode('utf-8')).decode('utf-8') + "```")
 @bot.command()
 async def decode16(ctx, *, message):
+    print("[DECODE16] " + str(datetime.datetime.now()))
     await ctx.send("```Decoded output: " + base64.b16decode(message.encode('utf-8')).decode('utf-8') + "```")
 @bot.command()
 async def encodea85(ctx, *, message):
+    print("[ENCODEA85] " + str(datetime.datetime.now()))
     await ctx.send("```Encoded output: " + base64.a85encode(message.encode('utf-8')).decode('utf-8') + "```")
 @bot.command()
 async def decodea85(ctx, *, message):
+    print("[DECODEA85] " + str(datetime.datetime.now()))
     await ctx.send("```Decoded output: " + base64.a85decode(message.encode('utf-8')).decode('utf-8') + "```")
 @bot.command()
+async def encode85(ctx, *, message):
+    print("[ENCODE85] " + str(datetime.datetime.now()))
+    await ctx.send("```Encoded output: " + base64.b85encode(message.encode('utf-8')).decode('utf-8') + "```")
+@bot.command()
+async def decode85(ctx, *, message):
+    print("[DECODE85] " + str(datetime.datetime.now()))
+    await ctx.send("```Decoded output: " + base64.b85decode(message.encode('utf-8')).decode('utf-8') + "```")
+@bot.command()
 async def info(ctx):
+    print("[INFO] " + str(datetime.datetime.now()))
     await ctx.send("```This bot was made by @Nucleus#1922.\nThe source code can be found here: https://github.com/EnterTheVoid-x86/Dencode```")
 @bot.command()
 async def ping(ctx):
+    print("[PING] " + str(datetime.datetime.now()))
     await ctx.send("```Pong!```")
 @bot.command()
 async def help(ctx):
-    await ctx.send("```Available commands:\n\n=encode64 <message>\n=decode64 <message>\n=encodebin <message>\n=decodebin <message>\n=encodehex <message>\n=decodehex <message>\n=experimentalencodeoct <message>\n=experimentaldecodeoct <message>\n=encode32 <message>\n=decode32 <message>\n=encode16 <message>\n=decode16 <message>\n=encodea85 <message>\n=decodea85 <message>\n=info\n=ping\n=help\n\nOctal encoding/decoding is experimental, and still has many bugs. For example,\nmessages encoded or decoded with spaces won't encode or decode properly.```")
+    await ctx.send("```Available commands:\n\n=encode64 <message>\n=decode64 <message>\n=encodebin <message>\n=decodebin <message>\n=encodehex <message>\n=decodehex <message>\n=experimentalencodeoct <message>\n=experimentaldecodeoct <message>\n=encode32 <message>\n=decode32 <message>\n=encode16 <message>\n=decode16 <message>\n=encodea85 <message>\n=decodea85 <message>\n=encode85 <message>\n=decode85 <message>\n=info\n=ping\n=help\n\nOctal encoding/decoding is experimental, and still has many bugs. For example,\nmessages encoded or decoded with spaces won't encode or decode properly.```")
 bot.run(token)
